2e245adfaa88544c3a5ef39ef6c63f0282d044a7
Woomymy <woomy@woomy.be>
2025-03-01 15:21:10 +0100

    connectivity: gps: Removed unused variables
    Change-Id: Ia74d36f558c545e7fece3ae9378531f4a2f7c4e5
    Signed-off-by: Woomymy <woomy@woomy.be>

--- drivers/misc/mediatek/connectivity/gps/lna_ctrl/src/gps_lna_drv.c
+++ drivers/misc/mediatek/connectivity/gps/lna_ctrl/src/gps_lna_drv.c
@@ -179,10 +179,6 @@ void gps_lna_pin_ctrl(enum gps_data_link_id_enum link_id, bool dsp_is_on, bool f
 
 static int gps_lna_probe(struct platform_device *pdev)
 {
-	struct resource *irq;
-	int i;
-	bool okay;
-
 	g_gps_lna_pinctrl_ptr = devm_pinctrl_get(&pdev->dev);
 	if (IS_ERR(g_gps_lna_pinctrl_ptr))
 		GPS_INFO_FUNC("devm_pinctrl_get fail");
