231144701f33f0d4236d06ad88513940355d1cca
Woomymy <woomy@woomy.be>
2025-09-13 13:18:42 +0200

    Revert "[ALPS05060526] UPSTREAM: mm: update get_user_pages_longterm"
    This reverts commit 3a0541b60f8c0780101dfeff4097d43c48998bf1.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1642,8 +1642,7 @@ long get_user_pages_locked(unsigned long start, unsigned long nr_pages,
 		    unsigned int gup_flags, struct page **pages, int *locked);
 long get_user_pages_unlocked(unsigned long start, unsigned long nr_pages,
 		    struct page **pages, unsigned int gup_flags);
-
-#if defined(CONFIG_FS_DAX) || defined(CONFIG_CMA)
+#ifdef CONFIG_FS_DAX
 long get_user_pages_longterm(unsigned long start, unsigned long nr_pages,
 			    unsigned int gup_flags, struct page **pages,
 			    struct vm_area_struct **vmas);
