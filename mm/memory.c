af5bec3bd515aaa4d407e415f17d27c558151ed1
Woomymy <woomy@woomy.be>
2025-09-13 13:38:08 +0200

    Revert "[ALPS04983389] spf: add pte unmap for proper error handling"
    This reverts commit 1e427d5e16aeca012dc672b2085a48c5760f6088.

--- mm/memory.c
+++ mm/memory.c
@@ -4256,10 +4256,8 @@ static vm_fault_t do_numa_page(struct vm_fault *vmf)
 	 * validation through pte_unmap_same(). It's of NUMA type but
 	 * the pfn may be screwed if the read is non atomic.
 	 */
-	if (!pte_spinlock(vmf)) {
-		pte_unmap(vmf->pte);
+	if (!pte_spinlock(vmf))
 		return VM_FAULT_RETRY;
-	}
 	if (unlikely(!pte_same(*vmf->pte, vmf->orig_pte))) {
 		pte_unmap_unlock(vmf->pte, vmf->ptl);
 		goto out;
@@ -4468,10 +4466,8 @@ static vm_fault_t handle_pte_fault(struct vm_fault *vmf)
 	if (pte_protnone(vmf->orig_pte) && vma_is_accessible(vmf->vma))
 		return do_numa_page(vmf);
 
-	if (!pte_spinlock(vmf)) {
-		pte_unmap(vmf->pte);
+	if (!pte_spinlock(vmf))
 		return VM_FAULT_RETRY;
-	}
 	entry = vmf->orig_pte;
 	if (unlikely(!pte_same(*vmf->pte, entry)))
 		goto unlock;
@@ -4821,16 +4817,11 @@ vm_fault_t __handle_speculative_fault(struct mm_struct *mm,
 	 * We need to re-validate the VMA after checking the bounds, otherwise
 	 * we might have a false positive on the bounds.
 	 */
-	if (read_seqcount_retry(&vma->vm_sequence, seq)) {
-		/* If leaving spf earilier, try to unmap the pte */
-		if (vmf.pte)
-			pte_unmap(vmf.pte);
+	if (read_seqcount_retry(&vma->vm_sequence, seq))
 		goto out_put;
-	}
 
 	mem_cgroup_enter_user_fault();
 	ret = handle_pte_fault(&vmf);
-	/* NOTE: vmf.pte should be unmapped after handle_pte_fault */
 	mem_cgroup_exit_user_fault();
 
 	put_vma(vma);
